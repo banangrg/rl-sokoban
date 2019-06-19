1a89e1499d96b56b9abd029df1d2cbd063bb6fd0
godemperor785 <godemperor785@gmail.com>
2019-06-19 20:44:49 +0200

    Add the final agent + some fixes and change in scaled env
    Signed-off-by: godemperor785 <godemperor785@gmail.com>

--- FinalDQN.py
+++ FinalDQN.py
@@ -3,16 +3,22 @@ import time
 import argparse
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Conv2D, Dense, Activation, MaxoutDense, LeakyReLU, PReLU
+from keras.layers import Flatten, Conv2D, Dense, Activation, MaxoutDense, LeakyReLU, PReLU, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
 from keras.optimizers import Adam
 from rl.agents.dqn import DQNAgent
+from rl.callbacks import ModelIntervalCheckpoint
 from rl.policy import BoltzmannQPolicy, EpsGreedyQPolicy
 from rl.memory import SequentialMemory
 from SokobanEnv import SokobanEnv
 from SokobanGame import SokobanGame
 from MemoryLoader import SokobanManualGameMemoryLoader
 from DQNAgentUtils import DimensionKillerProcessor
-from DQNAgentUtils import save_agent_weights_and_summary_to_file, show_reward_plot, load_agent_weights
+from DQNAgentUtils import save_agent_weights_and_summary_to_file, show_reward_plot, load_agent_weights, PATH_TO_SAVED_MODELS
+
+
+# to use BatchNormalization with channels_first this commit is needed to be added manually:
+# https://github.com/keras-team/keras/commit/e3a2f7d29f2f1c21ecc978bd0038b1d1330d33c2
 
 
 NO_TEST = '__no_test__'
@@ -22,6 +28,7 @@ NUMBER_OF_POSSIBLE_ACTIONS = 4
 MEMORY_LIMIT = 1 * (10 ** 6)
 TOTAL_NUMBER_OF_STEPS = 6 * (10 ** 6)
 NUMBER_OF_STEPS_FOR_EXPLORATION = 1 * (10 ** 6)
+DEFAULT_CALLBACK_INTERVAL = 1 * (10 ** 6)
 WINDOW_LENGTH = 1
 VERBOSITY_LEVEL = 1
 AFTER_HOW_MANY_GAMES_PRINT_VICTORY_STATS = 500
@@ -44,7 +51,7 @@ LOADED_GAMES_MEMORY_LIMIT = MEMORY_LIMIT
 
 NUMBER_OF_STEPS_FOR_WARMUP = 50000
 
-NUMBER_OF_EPISODES_FOR_TESTING = 500
+NUMBER_OF_EPISODES_FOR_TESTING = 1000
 
 SAVE_EVERY_GAME = False
 
@@ -77,9 +84,73 @@ def make_custom_model(input_rows, input_cols):
     return custom_model
 
 
+def make_custom_model_3_norm(input_rows, input_cols):
+    """ roughly based on VGG and this: https://www.scitepress.org/papers/2018/67520/67520.pdf
+        It would probably be better to save model of this network more often with -ci parameter set to 100k or even less
+    """
+    try:
+        if not args.scale_env:
+            raise ValueError('This network requires Scaled Env! Use -sc option!')
+    except NameError:
+        pass
+
+    global HIDDEN_ACTIVATION
+
+    custom_model = Sequential()
+    custom_model.add(Conv2D(64, kernel_size=(3, 3), input_shape=(WINDOW_LENGTH, input_rows, input_cols),
+                            data_format='channels_first'))
+    custom_model.add(BatchNormalization(axis=1))  # axis=1 due to Conv2D with channels_first
+    custom_model.add(get_hidden_layer_activation(HIDDEN_ACTIVATION))
+    custom_model.add(Conv2D(64, kernel_size=(3, 3), data_format='channels_first'))
+    custom_model.add(BatchNormalization(axis=1))
+    custom_model.add(get_hidden_layer_activation(HIDDEN_ACTIVATION))
+
+    custom_model.add(Conv2D(128, kernel_size=(3, 3), data_format='channels_first'))
+    custom_model.add(BatchNormalization(axis=1))
+    custom_model.add(get_hidden_layer_activation(HIDDEN_ACTIVATION))
+    custom_model.add(Conv2D(128, kernel_size=(3, 3), data_format='channels_first'))
+    custom_model.add(BatchNormalization(axis=1))
+    custom_model.add(get_hidden_layer_activation(HIDDEN_ACTIVATION))
+
+    custom_model.add(Conv2D(256, kernel_size=(3, 3), data_format='channels_first'))
+    custom_model.add(BatchNormalization(axis=1))
+    custom_model.add(get_hidden_layer_activation(HIDDEN_ACTIVATION))
+    custom_model.add(Conv2D(256, kernel_size=(3, 3), data_format='channels_first'))
+    custom_model.add(BatchNormalization(axis=1))
+    custom_model.add(get_hidden_layer_activation(HIDDEN_ACTIVATION))
+    custom_model.add(Conv2D(256, kernel_size=(3, 3), data_format='channels_first'))
+    custom_model.add(BatchNormalization(axis=1))
+    custom_model.add(get_hidden_layer_activation(HIDDEN_ACTIVATION))
+
+    custom_model.add(Conv2D(512, kernel_size=(3, 3), data_format='channels_first'))
+    custom_model.add(BatchNormalization(axis=1))
+    custom_model.add(get_hidden_layer_activation(HIDDEN_ACTIVATION))
+    custom_model.add(Conv2D(512, kernel_size=(3, 3), data_format='channels_first'))
+    custom_model.add(BatchNormalization(axis=1))
+    custom_model.add(get_hidden_layer_activation(HIDDEN_ACTIVATION))
+    custom_model.add(Conv2D(512, kernel_size=(3, 3), data_format='channels_first'))
+    custom_model.add(BatchNormalization(axis=1))
+    custom_model.add(get_hidden_layer_activation(HIDDEN_ACTIVATION))
+
+    custom_model.add(Conv2D(512, kernel_size=(3, 3), data_format='channels_first'))
+    custom_model.add(BatchNormalization(axis=1))
+    custom_model.add(get_hidden_layer_activation(HIDDEN_ACTIVATION))
+    custom_model.add(Conv2D(512, kernel_size=(3, 3), data_format='channels_first'))
+    custom_model.add(BatchNormalization(axis=1))
+    custom_model.add(get_hidden_layer_activation(HIDDEN_ACTIVATION))
+
+    custom_model.add(Flatten())
+    custom_model.add(Dense(384))
+    custom_model.add(BatchNormalization())
+    custom_model.add(get_hidden_layer_activation(HIDDEN_ACTIVATION))
+    custom_model.add(MaxoutDense(NUMBER_OF_POSSIBLE_ACTIONS, nb_feature=4))
+    custom_model.add(Activation('linear'))
+    return custom_model
+
+
 def make_custom_model_2(input_rows, input_cols):
     global HIDDEN_ACTIVATION
-    #HIDDEN_ACTIVATION = 'relu'
+
     custom_model = Sequential()
     custom_model.add(Conv2D(int(CONV_LAYER_SIZE_BASE / 2), kernel_size=(7, 7), strides=2,
                             input_shape=(WINDOW_LENGTH, input_rows, input_cols), data_format='channels_first'))
@@ -96,7 +167,30 @@ def make_custom_model_2(input_rows, input_cols):
     return custom_model
 
 
-def get_new_sokoban_env(for_test: bool, is_first_training: bool, use_generated_maps: bool):
+def make_custom_model_2_norm(input_rows, input_cols):
+    global HIDDEN_ACTIVATION
+
+    custom_model = Sequential()
+    custom_model.add(Conv2D(int(CONV_LAYER_SIZE_BASE / 2), kernel_size=(7, 7), strides=2,
+                            input_shape=(WINDOW_LENGTH, input_rows, input_cols), data_format='channels_first'))
+    custom_model.add(BatchNormalization(axis=1))    # axis=1 due to Conv2D with channels_first
+    custom_model.add(get_hidden_layer_activation(HIDDEN_ACTIVATION))
+    custom_model.add(Conv2D(CONV_LAYER_SIZE_BASE, kernel_size=(5, 5), data_format='channels_first'))
+    custom_model.add(BatchNormalization(axis=1))
+    custom_model.add(get_hidden_layer_activation(HIDDEN_ACTIVATION))
+    custom_model.add(Conv2D(CONV_LAYER_SIZE_BASE, kernel_size=(3, 3), data_format='channels_first'))
+    custom_model.add(BatchNormalization(axis=1))
+    custom_model.add(get_hidden_layer_activation(HIDDEN_ACTIVATION))
+    custom_model.add(Flatten())
+    custom_model.add(Dense(512))
+    #custom_model.add(BatchNormalization())
+    custom_model.add(get_hidden_layer_activation(HIDDEN_ACTIVATION))
+    custom_model.add(MaxoutDense(NUMBER_OF_POSSIBLE_ACTIONS, nb_feature=4))
+    custom_model.add(Activation('linear'))
+    return custom_model
+
+
+def get_new_sokoban_env(for_test: bool, is_first_training: bool, use_generated_maps: bool, use_scaled_env: bool = False):
     SokobanEnv.configure_env_size(new_rows=ENV_SIZE_ROWS, new_cols=ENV_SIZE_COLS)
     if use_generated_maps:
         map_choice_option = SokobanEnv.USE_GENERATED_MAPS
@@ -116,7 +210,7 @@ def get_new_sokoban_env(for_test: bool, is_first_training: bool, use_generated_m
                                use_more_than_one_channel=False,
                                scale_rewards=False,
                                scale_range=(-1, 1),
-                               use_scaled_env_representation=False,
+                               use_scaled_env_representation=use_scaled_env,
                                disable_map_rotation=False)
         return agent_env
     elif not for_test and not is_first_training:
@@ -131,7 +225,7 @@ def get_new_sokoban_env(for_test: bool, is_first_training: bool, use_generated_m
                                use_more_than_one_channel=False,
                                scale_rewards=False,
                                scale_range=(-1, 1),
-                               use_scaled_env_representation=False,
+                               use_scaled_env_representation=use_scaled_env,
                                disable_map_rotation=False)
         return agent_env
     else:   # for testing
@@ -145,7 +239,7 @@ def get_new_sokoban_env(for_test: bool, is_first_training: bool, use_generated_m
                                use_more_than_one_channel=False,
                                scale_rewards=False,
                                scale_range=(-1, 1),
-                               use_scaled_env_representation=False,
+                               use_scaled_env_representation=use_scaled_env,
                                disable_map_rotation=False)
         return agent_env
 
@@ -170,6 +264,15 @@ if __name__ == "__main__":
     args_parser.add_argument("-g", "--generated_maps", help="if set agent will train/test on generated maps. " +
                                                             "Stats saving is disabled due to not having static map names",
                              action="store_true", dest="generated_maps")
+    args_parser.add_argument("-sc", "--scale_env", help="if set will use scaled env representation",
+                             action="store_true", dest="scale_env")
+    args_parser.add_argument("-ci", "--callback_interval", type=int,
+                             help="number of steps after which callbacks will be called during training",
+                             dest="callback_interval", default=DEFAULT_CALLBACK_INTERVAL)
+    args_parser.add_argument("-m", "--model_type", type=str,
+                             help="type of loaded model 1 - default, 2 diffconv, 2n diffconv with batch normalization, 3n - final network",
+                             choices=['1', '2', '2n', '3n'],
+                             dest="model_type", default='3n')
     args = args_parser.parse_args()
     if args.test_weights != NO_TEST:
         weights_file_name = args.test_weights
@@ -199,10 +302,20 @@ if __name__ == "__main__":
 
     bugfix_processor = DimensionKillerProcessor()
 
-    env = get_new_sokoban_env(for_test=is_test, is_first_training=args.is_first_traning, use_generated_maps=use_generated_maps)
+    env = get_new_sokoban_env(for_test=is_test, is_first_training=args.is_first_traning,
+                              use_generated_maps=use_generated_maps, use_scaled_env=args.scale_env)
 
     print("[INFO] Building model...")
-    model = make_custom_model(input_cols=ENV_SIZE_COLS, input_rows=ENV_SIZE_ROWS)
+    if args.model_type == '1':
+        model = make_custom_model(input_cols=ENV_SIZE_COLS, input_rows=ENV_SIZE_ROWS)
+    elif args.model_type == '2':
+        model = make_custom_model_2(input_cols=ENV_SIZE_COLS, input_rows=ENV_SIZE_ROWS)
+    elif args.model_type == '2n':
+        model = make_custom_model_2_norm(input_cols=ENV_SIZE_COLS, input_rows=ENV_SIZE_ROWS)
+    elif args.model_type == '3n':
+        model = make_custom_model_3_norm(input_cols=ENV_SIZE_COLS, input_rows=ENV_SIZE_ROWS)
+    else:
+        raise ValueError('Unknown model type!')
     model.summary()
 
     if DO_LOAD_GAMES_FROM_FILES and not is_test:
@@ -212,21 +325,25 @@ if __name__ == "__main__":
     basic_memory = SequentialMemory(limit=MEMORY_LIMIT, window_length=WINDOW_LENGTH)
     action_choice_policy = BoltzmannQPolicy(tau=1., clip=(-500., 500.))
     #action_choice_policy = EpsGreedyQPolicy(eps=0.1)
+
+    # maybe train_interval=1 ?
     dqn = DQNAgent(model=model, nb_actions=NUMBER_OF_POSSIBLE_ACTIONS, policy=action_choice_policy, memory=basic_memory,
                    processor=bugfix_processor, batch_size=MEMORY_REPLAY_BATCH_SIZE, test_policy=action_choice_policy,
                    enable_double_dqn=True, enable_dueling_network=True, nb_steps_warmup=NUMBER_OF_STEPS_FOR_WARMUP,
                    gamma=GAMMA, target_model_update=10000, train_interval=4, delta_clip=1.)
-    opt = Adam(lr=.00025)
+    opt = Adam(lr=.00025, clipnorm=1.0)     # clipnorm so that there are no exploding gradients
     dqn.compile(optimizer=opt, metrics=['mae'])
 
     if is_test:
         print("[INFO] Loading weights from file: " + weights_file_name)
         load_agent_weights(dqn, weights_file_name)
+        print("[INFO] Done loading")
         do_train = False
     elif not args.is_first_traning:
         file_name = args.training_weights
         print("[INFO] Loading weights from file: " + file_name)
         load_agent_weights(dqn, file_name)
+        print("[INFO] Done loading")
         do_train = True
     else:
         print("[INFO] beginning first training")
@@ -239,9 +356,13 @@ if __name__ == "__main__":
         game_record_loader.load_all_games()
 
     if do_train:
+        # save model weights after N steps
+        checkpoint_weights_filename = PATH_TO_SAVED_MODELS + '_checkpoint_dqn_weights_{step}.h5f'
+        callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=args.callback_interval)]
+
         print("[INFO] Training...")
         training_history = dqn.fit(env, nb_steps=number_of_steps, verbose=VERBOSITY_LEVEL, nb_max_episode_steps=10000,
-                                   log_interval=VERBOSITY_1_LOGGER_INTERVAL)
+                                   log_interval=VERBOSITY_1_LOGGER_INTERVAL, callbacks=callbacks)
         # save weights to file
         print("Saving dqn weights and stats")
         save_agent_weights_and_summary_to_file(base_file_name="final_DQN_",
@@ -265,6 +386,10 @@ if __name__ == "__main__":
     print("Done saving stats")
 
     end_time = time.time()
+
+    if do_train:
+        show_reward_plot(training_history, plot_title="DQN_v1")
+
     execution_time = end_time - start_time
     print(" >>>>>> ALL DONE. It took " + str(execution_time) + " seconds = " + str(execution_time / 60.0) + " minutes = " + str((execution_time / 60.0) / 60.0) + " hours")
     sys.exit(0)  # to free process memory immediately
